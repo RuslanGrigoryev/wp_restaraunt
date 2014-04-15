a94ed0fd1bb048fcbdba663e8e7829ce9fd1dea9
RuslanGrigoryev <rus.grigoryev@yandex.ua>
2014-04-15 19:53:50 +0300

    Try 3

--- wp-content/themes/restaurant/index.php
+++ wp-content/themes/restaurant/index.php
@@ -17,7 +17,7 @@
 get_header(); ?>   
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
-		<?php if ( have_posts() ) : ?>
+		<?php if ( have_posts() ) : ?> 
 
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
