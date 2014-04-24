6644dd49ce2a4b868e46cd4df6372b6cc926c39d
RuslanGrigoryev <rus.grigoryev@yandex.ua>
2014-04-24 21:55:13 +0300

    Добавил страницу видео

--- wp-content/themes/restaurant/blog.php
+++ wp-content/themes/restaurant/blog.php
@@ -11,6 +11,7 @@ if ( have_posts() ) : // если имеются записи в блоге.
   query_posts('cat=5');   // указываем ID рубрик, которые необходимо вывести.
   while (have_posts()) : the_post();  // запускаем цикл обхода материалов блога
 ?>
+<div class="blog-page">
 <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 		<?php the_content();
 	
@@ -18,5 +19,5 @@ if ( have_posts() ) : // если имеются записи в блоге.
 endif;
 /* Сбрасываем настройки цикла. Если ниже по коду будет идти еще один цикл, чтобы не было сбоя. */
 wp_reset_query();                
-?>
+?></div>
 <?php get_footer(); ?>
---
+++ wp-content/themes/restaurant/video.php
@@ -0,0 +1,24 @@
+﻿<?php
+/*
+Template Name: Videos Page
+*/
+?>
+<?php get_header(); ?>   
+
+	<?php
+		if ( have_posts() ) : // если имеются записи в блоге.
+		  query_posts('cat=7');   // указываем ID рубрик, которые необходимо вывести.
+		  while (have_posts()) : the_post();  // запускаем цикл обхода материалов блога
+		?>
+		<div class="video-page">
+		<h1><?php the_title(); ?></a></h1>
+				<?php the_content();
+			
+		  endwhile;  // завершаем цикл.
+		endif;
+		/* Сбрасываем настройки цикла. Если ниже по коду будет идти еще один цикл, чтобы не было сбоя. */
+		wp_reset_query();                
+	?>
+	</div>
+
+<?php get_footer(); ?>
