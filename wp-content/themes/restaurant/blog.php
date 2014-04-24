fc238e21a995241e76dbb7ca82ce3b9aa631528a
RuslanGrigoryev <rus.grigoryev@yandex.ua>
2014-04-24 18:46:09 +0300

    Тест блога

---
+++ wp-content/themes/restaurant/blog.php
@@ -0,0 +1,21 @@
+﻿<?php
+/*
+Template Name: Blog Page
+*/
+?>
+<?php get_header(); ?>   
+
+<?php
+if ( have_posts() ) : // если имеются записи в блоге.
+  query_posts('cat=5');   // указываем ID рубрик, которые необходимо вывести.
+  while (have_posts()) : the_post();  // запускаем цикл обхода материалов блога
+?>
+<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+<?php the_content();
+  endwhile;  // завершаем цикл.
+endif;
+/* Сбрасываем настройки цикла. Если ниже по коду будет идти еще один цикл, чтобы не было сбоя. */
+wp_reset_query();                
+?>
+
+<?php get_footer(); ?>
