7028235318b4f449e0e922165cab04c4220b7536
RuslanGrigoryev <rus.grigoryev@yandex.ua>
2014-05-13 18:13:01 +0300

    test re

--- wp-content/themes/restaurant/blog.php
+++ wp-content/themes/restaurant/blog.php
@@ -12,8 +12,8 @@ if ( have_posts() ) : // если имеются записи в блоге.
   while (have_posts()) : the_post();  // запускаем цикл обхода материалов блога
 ?>
 <div class="blog-page">
-<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-		<?php the_content();
+<h1>das	<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+		<?php the_content(); 
 	
   endwhile;  // завершаем цикл.
 endif;
