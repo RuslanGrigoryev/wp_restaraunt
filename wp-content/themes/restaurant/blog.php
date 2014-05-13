4a1646c679a6d1d447349e0cddeeb4c99125c501
RuslanGrigoryev <rus.grigoryev@yandex.ua>
2014-05-13 19:25:02 +0300

    Вывод сокращенных записей в категоориях

--- wp-content/themes/restaurant/blog.php
+++ wp-content/themes/restaurant/blog.php
@@ -3,6 +3,12 @@
 Template Name: Blog Page
 */
 ?>
-<?php get_header(); ?>   
-
+<?php get_header(); ?> 
+<?php
+query_posts('cat=5');
+if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+  <article class="article-content">	"<?php the_excerpt(); ?> <a href="<?php the_permalink() ?>">Читать полностью &raquo;</a>"</article>
+<?php endwhile; endif;
+wp_reset_query();
+?>
 <?php get_footer(); ?>
